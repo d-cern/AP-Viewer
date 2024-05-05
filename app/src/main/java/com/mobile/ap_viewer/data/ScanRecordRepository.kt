f7d12d0e8daa6db71e239e84187666fa146e8f05
d-cern <duarte.cernadas@gmail.com>
2024-05-05 09:00:15 -0500

    Create and implement ScanRecordRepository

---
+++ app/src/main/java/com/mobile/ap_viewer/data/ScanRecordRepository.kt
@@ -0,0 +1,43 @@
+package com.mobile.ap_viewer.data
+
+import android.location.Location
+import com.mobile.ap_viewer.data.model.AccessPoint
+import com.mobile.ap_viewer.data.model.ScanRecord
+import javax.inject.Inject
+
+class ScanRecordRepository @Inject constructor(
+    private val wifi: WiFiManagerDataSource,
+    private val location: LocationManagerDataSource
+) {
+    private var accessPointList: MutableList<AccessPoint> = mutableListOf<AccessPoint>()
+    private var userLocation: Location? = null
+    private lateinit var scanRecord: ScanRecord
+
+    init {
+        refreshScanRecords()
+    }
+
+    fun refreshScanRecords() {
+        val results = wifi.getScanResults()
+        accessPointList.clear()
+        results.forEach {
+            accessPointList
+                .add(
+                    AccessPoint(
+                        it.BSSID,
+                        it.SSID,
+                        it.capabilities,
+                        it.channelWidth,
+                        it.frequency,
+                        it.level,
+                        it.timestamp
+                    )
+                )
+        }
+        scanRecord = ScanRecord(accessPointList, userLocation)
+    }
+
+    fun getScanRecord(): ScanRecord {
+        return scanRecord
+    }
+}
