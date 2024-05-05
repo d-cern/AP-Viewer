267c9d534223c46956537bb6a905f09599a1634b
d-cern <duarte.cernadas@gmail.com>
2024-05-05 10:56:10 -0500

    Revert "Added Start Screen UI and Access Point object"
    This reverts commit 6d97c5e3858b58f29c76fd8a8b3df89cc117cce4.

--- app/src/main/java/com/mobile/ap_viewer/MainActivity.kt
+++ app/src/main/java/com/mobile/ap_viewer/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.mobile.ap_viewer
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,36 +11,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mobile.ap_viewer.ui.theme.APViewerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-
-        val scanButton = findViewById<Button>(R.id.ScanButton)
-        scanButton.setOnClickListener {
-            Toast.makeText(this, "Scanning...", Toast.LENGTH_SHORT).show()
-        }
-
-        val optionsButton = findViewById<Button>(R.id.OptionsButton)
-        /*setContent {
+        setContent {
             APViewerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //Greeting("Android")
+                    Greeting("Android")
                 }
             }
-        }*/
+        }
     }
 }
 
-/*@Composable
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name! How are you doing?",
+        text = "Hello $name!",
         modifier = modifier
     )
 }
@@ -53,4 +45,4 @@ fun GreetingPreview() {
     APViewerTheme {
         Greeting("Android")
     }
-}*/
+}
