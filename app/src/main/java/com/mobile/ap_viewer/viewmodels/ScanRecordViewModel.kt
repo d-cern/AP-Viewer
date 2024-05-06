08299ce45ba00280eb181f4c48f9e79d08a78bda
d-cern <duarte.cernadas@gmail.com>
2024-05-05 20:56:19 -0500

    Create ScanRecordViewModel

--- app/src/main/java/com/mobile/ap_viewer/viewmodels/ScanRecordViewModel.kt
+++ app/src/main/java/com/mobile/ap_viewer/viewmodels/ScanRecordViewModel.kt
@@ -1,4 +1,40 @@
 package com.mobile.ap_viewer.viewmodels
 
-class ScanRecordViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mobile.ap_viewer.data.ScanRecordRepository
+import com.mobile.ap_viewer.data.model.AccessPoint
+import com.mobile.ap_viewer.data.model.ScanRecord
+import com.mobile.ap_viewer.di.DefaultDispatcher
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+@HiltViewModel
+class ScanRecordViewModel @Inject internal constructor(
+    private val scanRecordRepository: ScanRecordRepository,
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher
+) : ViewModel() {
+
+    private val _accessPoints = MutableLiveData<List<AccessPoint>>()
+    val accessPoints: LiveData<List<AccessPoint>>
+        get() = _accessPoints
+
+    init {
+        _accessPoints.value = scanRecordRepository.getAccessPoints()
+    }
+
+    fun updateAccessPoints() {
+        // coroutine - update access point list on another thread
+        viewModelScope.launch {
+            val newAccessPoints = withContext(defaultDispatcher) {
+                scanRecordRepository.getAccessPoints()
+            }
+            _accessPoints.value = newAccessPoints
+        }
+    }
 }
